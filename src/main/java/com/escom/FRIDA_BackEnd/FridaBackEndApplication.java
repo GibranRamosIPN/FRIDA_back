f96b25583862322de9ee4554058b2e95a9b6ec64
Gibran Ramos <gramosg1300@alumno.ipn.mx>
2021-01-06 17:26:21 -0600

    Aún en configuración

--- src/main/java/com/escom/FRIDA_BackEnd/FridaBackEndApplication.java
+++ src/main/java/com/escom/FRIDA_BackEnd/FridaBackEndApplication.java
@@ -3,11 +3,12 @@ package com.escom.FRIDA_BackEnd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.escom.FRIDA_BackEnd", "com.escom.FRIDA_BackEnd.Entity",
+"com.escom.FRIDA_BackEnd.Repository", "com.escom.FRIDA_BackEnd.Controller", "com.escom.FRIDA_BackEnd.Service"})
 public class FridaBackEndApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FridaBackEndApplication.class, args);
+            SpringApplication.run(FridaBackEndApplication.class, args);
 	}
 
 }
